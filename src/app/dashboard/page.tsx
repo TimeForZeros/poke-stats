6b46c90c79186c2084c48aa7b507462852432ede
Victor Gonzalez <dazedbutamused@gmail.com>
2025-08-23 13:52:56 -0700

    listing pokemon

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import React, { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import { PokemonEntry } from './pokeTable';
+import PokeCard from './poke-card';
 
-const POKE_API = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+const POKE_API = 'https://pokeapi.co/api/v2/pokemon?limit=4';
 
-const PokeTable = dynamic(() => import('./pokeTable'));
+// const PokeTable = dynamic(() => import('./pokeTable'));
 
 const Dashboard = async () => {
   const res = await fetch(POKE_API);
@@ -13,7 +14,12 @@ const Dashboard = async () => {
     <div>
       <div>test</div>
       <Suspense fallback={'Loading....'}>
-        <PokeTable data={data} />
+        <div className='flex'>
+        {data.map((entry) => (
+          <PokeCard key={entry.name} name={entry.name} url={entry.url} />
+        ))}
+        {/* <PokeTable data={data} /> */}
+        </div>
       </Suspense>
     </div>
   );
---
+++ src/app/dashboard/poke-card.tsx
@@ -0,0 +1,38 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Image from 'next/image';
+
+type CardProps = {
+  name: string;
+  url: string;
+};
+
+const PokeCard = async ({ name, url }: CardProps) => {
+  const data = await fetch(url);
+  const pokemon = await data.json();
+
+  return (
+    <Card className='w-[12rem] '>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+        <CardAction>Card Action</CardAction>
+      </CardHeader>
+      <CardContent className='justify-center flex'>
+        <Image src={pokemon.sprites.front_default} alt={name} height={100} width={100} />
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default PokeCard;
